570e966a5e9fb69817daf706cc7e26ccb9da837b
walter <walter8098@gmail.com>
2016-02-18 10:24:42 +0800

    添加ZipkinTracer.cs

---
+++ Zipkin.Tracer.Core/ZipkinTracer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zipkin.Tracer.Core
+{
+    public static class ZipkinTracer
+    {
+        private static ZipkinWrapper zipkin;
+
+        public static void Initialize(ZipkinWrapper zipkinWrapper)
+        {
+            zipkin = zipkinWrapper;
+        }
+
+        public static void RecordServer(string value)
+        {
+            zipkin.ServerTracer().SubmitAnnotation(value);
+        }
+
+        public static void RecordServer(string name, string value)
+        {
+            zipkin.ServerTracer().SubmitBinaryAnnotation(name, value);
+        }
+
+        public static void RecordClient(string value)
+ 	    {
+            zipkin.ClientTracer().SubmitAnnotation(value);
+ 	    }
+
+        public static void RecordClient(string name, string value)
+        {
+            zipkin.ClientTracer().SubmitBinaryAnnotation(name, value);
+        }
+
+        public static void StartClientTracer(string requestName)
+ 	    {
+            zipkin.ClientTracer().StartNewSpan(requestName);
+            zipkin.ClientTracer().SetClientSent();
+ 	    }
+
+        public static void StopClientTracer()
+        {
+            zipkin.ClientTracer().SetClientReceived();
+        }
+
+        public static void StartLocalTracer(string component, string operation)
+        {
+            zipkin.LocalTracer().StartNewSpan(component, operation);
+        }
+
+        public static void StopLocalTracer()
+        {
+            zipkin.LocalTracer().FinishSpan();
+        }
+    }
+}
